c4f31c7e1018a2febe1a6035bb0e0b8ddc8b82ba
Bermon Painter <hello@bermonpainter.com>
2013-06-08 15:17:45 -0400

    Added 5 new speakers (Jason, Jina, Rebecca, Rik, & Tim)
    - Updated layout on mobile. There was too much L/R padding making the primary call to action and hexagons not fill well in the viewport
    - Fixed a bug where tapping a speaker image scrolled you to the top of the page
    - Added analytics tracking on external links

--- source/assets/javascripts/base.js
+++ source/assets/javascripts/base.js
@@ -1,39 +1,25 @@
 jQuery(document).ready(function($) {
-	
-	// Filter units and rooms based of their category
-	$('#units-nav, #rooms-nav').find('a').click(function(){
-		var $this = $(this);
-		var category = $this.data('category');
-		var $content = $('#content');
-
-		// remove .selected from the HTML
-		$this.parent().parent().find('a').removeClass('selected');
-
-		// add .selected class to this element
-		$this.addClass('selected');
-
-		// check to see if the link has a category
-		// so show or if it should show all categories
-		if (category == undefined || category == "") {
-			$content.find('.unit-box').show();
-		} else {
-			$content.find('.unit-box').hide();
-			$content.find("[data-categories~='" + category + "']").show();
-		}
-		
+	$('#speakers').find('a').click(function(){
 		return false;
 	});
 
-	// Makes units & rooms boxes clickable
-	var $units = $('#units').find('#content');
-	var $rooms = $('#rooms').find('#content');
-	$units.find('.unit-box').click(function(){
-		window.location = 'rooms/';
-		return false;
-	});
-	$rooms.find('.unit-box').click(function(){
-		window.location = 'patients/';
-		return false;
-	});
+	$("a[data-type]").on('click',function(e){
+        var link = $(this).attr("href");
+        var outboundLinkType = $(this).data("type");
 
+        if (outboundLinkType == undefined || outboundLinkType == "") {
+        	outboundLinkType = "External";
+        }
+
+        if (e.currentTarget.host != window.location.host) {
+            _gat._getTrackerByName()._trackEvent(outboundLinkType, e.currentTarget, link, 0);
+            if (e.metaKey || e.ctrlKey) {
+                var newtab = true;
+            }
+            if (!newtab) {
+                e.preventDefault();
+                setTimeout('document.location = "' + link + '"', 100);
+            }
+        }
+    });
 });
