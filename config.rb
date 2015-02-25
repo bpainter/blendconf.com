f6c66046918fa909e77f5dae119013bf76488307
Bermon Painter <hello@bermonpainter.com>
2015-02-25 08:46:09 -0500

    Added some media query love to fix vertical height issues. Smush those images.

--- config.rb
+++ config.rb
@@ -132,10 +132,10 @@ set :images_dir, 'assets/images'
 # ----------------------------------------------
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
@@ -149,8 +149,8 @@ configure :build do
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
+  require "middleman-smusher"
+  activate :smusher
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
